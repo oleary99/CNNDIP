14ea4f9cb977002f89d7d9a7eb545fc129222b95
Xyphold <58009876+BrendanBetterman@users.noreply.github.com>
2022-12-01 11:22:41 -0600

    Uses Dataset now
    updated so the cnn can use the data

--- CNNDIP/CNN.py
+++ CNNDIP/CNN.py
@@ -4,19 +4,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import PIL.Image as im
+import os
 
-(X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
+base_dir = '/afhq'
+#houston
+#train_dir = "C:\\Users\\Houston\\Downloads\\archive (1)\\afhq\\train"
+#test_dir = "C:\\Users\\Houston\\Downloads\\archive (1)\\afhq\\val"
+#windows
+#train_dir = "..\\..\\afhq\\train" 
+#test_dir = "..\\..\\afhq\\val"
+#linux
+train_dir = "../../afhq/train"
+test_dir = "../../afhq/val"
 
-image = X_train[785]
-data = im.fromarray(image)
-imgplot = plt.imshow(data)
-plt.show()
+training_set = tf.keras.preprocessing.image_dataset_from_directory(
+train_dir,
+seed=101,
+image_size=(200, 200),
+batch_size=32)
 
-X_train = X_train / 255
-X_test = X_test / 255
+class_names = training_set.class_names
+
+validation_set = tf.keras.preprocessing.image_dataset_from_directory(
+test_dir,
+seed=101,
+image_size=(200, 200),
+batch_size=32)
+
+data_augmentation = tf.keras.Sequential(
+  [
+    tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal", input_shape=(200, 200,3)),
+    tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+    tf.keras.layers.experimental.preprocessing.RandomZoom(0.2),
+  ]
+)
+
+#(X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
 model = tf.keras.Sequential(
     [
+    data_augmentation,
+    tf.keras.layers.experimental.preprocessing.Rescaling(1./255),
     tf.keras.layers.Conv2D(32, (3,3), padding='same', activation="relu",input_shape=(32, 32, 3)),
     tf.keras.layers.MaxPooling2D((2, 2), strides=2),
 
@@ -25,15 +53,15 @@ model = tf.keras.Sequential(
 
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(100, activation="relu"),
-    tf.keras.layers.Dense(10, activation="softmax")
+    tf.keras.layers.Dense(1, activation='sigmoid')
 ]
 )
 
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=[tf.keras.metrics.BinaryAccuracy()])
 
-history = model.fit(X_train,y_train, epochs=10,validation_data=(X_test,y_test))
+history = model.fit(training_set,validation_data=validation_set, epochs=10)
 
 metrics_df = pd.DataFrame(history.history)
 metrics_df[["loss","val_loss"]].plot();
