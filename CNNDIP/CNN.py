de0fcc36c38642c49597781d77fc2ca5bc07e617
BrendanBetterman <58009876+BrendanBetterman@users.noreply.github.com>
2022-12-06 15:37:34 -0600

    Refactored
    Refactored code so its modular and more read able. Made everything that could be a function into a function.

--- CNNDIP/CNN.py
+++ CNNDIP/CNN.py
@@ -6,63 +6,89 @@ import matplotlib.image as mpimg
 import PIL.Image as im
 import os
 
-base_dir = '/afhq'
-#houston
-#train_dir = "C:\\Users\\Houston\\Downloads\\archive (1)\\afhq\\train"
-#test_dir = "C:\\Users\\Houston\\Downloads\\archive (1)\\afhq\\val"
-#windows
-#train_dir = "..\\..\\afhq\\train" 
-#test_dir = "..\\..\\afhq\\val"
-#linux
-train_dir = "../../afhq/train"
-test_dir = "../../afhq/val"
 
-training_set = tf.keras.preprocessing.image_dataset_from_directory(
-train_dir,
-seed=101,
-image_size=(200, 200),
-batch_size=32)
+def setup_threads():
+  num_threads = 12
+  os.environ["OMP_NUM_THREADS"] = "12"
+  os.environ["TF_NUM_INTRAOP_THREADS"] = "12"
+  os.environ["TF_NUM_INTEROP_THREADS"] = "12"
 
-class_names = training_set.class_names
-
-validation_set = tf.keras.preprocessing.image_dataset_from_directory(
-test_dir,
-seed=101,
-image_size=(200, 200),
-batch_size=32)
-
-data_augmentation = tf.keras.Sequential(
-  [
-    tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal", input_shape=(200, 200,3)),
-    tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-    tf.keras.layers.experimental.preprocessing.RandomZoom(0.2),
-  ]
-)
-
-#(X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
-
-model = tf.keras.Sequential(
+  tf.config.threading.set_inter_op_parallelism_threads(
+      num_threads
+  )
+  tf.config.threading.set_intra_op_parallelism_threads(
+      num_threads
+  )
+  tf.config.set_soft_device_placement(True)
+def setup_training():
+  base_dir = '/afhq'
+  train_dir = "..\\..\\afhq\\train"
+  training_set = tf.keras.preprocessing.image_dataset_from_directory(
+  train_dir,
+  seed=101,
+  image_size=(200, 200),
+  batch_size=32)
+  class_names = training_set.class_names
+  return training_set
+def setup_validation():
+  test_dir = "..\\..\\afhq\\val"
+  validation_set = tf.keras.preprocessing.image_dataset_from_directory(
+  test_dir,
+  seed=101,
+  image_size=(200, 200),
+  batch_size=32)
+  return validation_set
+def setup_layer():
+  data_augmentation = tf.keras.Sequential(
     [
-    data_augmentation,
-    tf.keras.layers.experimental.preprocessing.Rescaling(1./255),
-    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation="relu",input_shape=(32, 32, 3)),
-    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+      tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal", input_shape=(200, 200,3)),
+      #tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+      #tf.keras.layers.experimental.preprocessing.RandomZoom(0.2),
+    ]
+  )
 
-    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation="relu"),
-    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+def relu_model():
+  return tf.keras.Sequential(
+      [
+      #data_augmentation,
+      tf.keras.layers.experimental.preprocessing.Rescaling(1/255),
+      tf.keras.layers.Conv2D(32, (3,3), padding='same', activation="relu",input_shape=(32, 32, 3)),
+      #tf.keras.layers.Conv2D(32, (3,3), padding='same', input_shape=(32, 32, 3)),
+      tf.keras.layers.MaxPooling2D((2, 2), strides=2),
 
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(100, activation="relu"),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-]
-)
+      tf.keras.layers.Conv2D(64, (3,3), padding='same', activation="relu"),
+      #tf.keras.layers.Conv2D(64, (3,3), padding='same'),
+      tf.keras.layers.MaxPooling2D((2, 2), strides=2),
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=[tf.keras.metrics.BinaryAccuracy()])
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(50, activation="relu"),
+      tf.keras.layers.Dropout(0.2),
+      tf.keras.layers.Dense(4, activation='softmax')
+  ]
+  )
+def compile_model(model,training_set,validation_set,epochs):
+  model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+  return model.fit(training_set,validation_data = validation_set, epochs=epochs)
+def graph_history(history):
+  training_loss = history.history['loss']
+  test_loss = history.history['val_loss']
+  epoch_count = range(1, len(training_loss) + 1)
+  plt.plot(epoch_count, training_loss, 'r--')
+  plt.plot(epoch_count, test_loss, 'b-')
+  plt.legend(['Training Loss', 'Test Loss'])
+  plt.xlabel('Epoch')
+  plt.ylabel('Loss')
+  plt.show();
 
-history = model.fit(training_set,validation_data=validation_set, epochs=10)
-
-metrics_df = pd.DataFrame(history.history)
-metrics_df[["loss","val_loss"]].plot();
-metrics_df[["accuracy","val_accuracy"]].plot();
+def main():
+  setup_threads()
+  training_set = setup_training()
+  validation_set = setup_validation()
+  setup_layer()
+  model = relu_model()
+  history = compile_model(model,training_set,validation_set,10)
+  graph_history(history)
+  
+main()
