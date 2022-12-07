b94d8f921ee5c730dfcb773bf6fb261cc8a4ba90
BrendanBetterman <58009876+BrendanBetterman@users.noreply.github.com>
2022-12-07 14:39:36 -0600

    Data parser with time now
    Time was forgotten on the previous parser.

--- CNNDIP/DataParser.py
+++ CNNDIP/DataParser.py
@@ -18,7 +18,7 @@ def main():
         #get loss accuracy and val loss and accuracy
         if ( count %2 ==1 and count %12 >2):
             print(line.strip().split(',')[0])
-        if (count % 2 == 1) and count%12 > 4:
+        if (count % 2 == 1) and count%12 > 2:
             s_line = line.strip()
             column = s_line.split(',')
             
@@ -31,19 +31,27 @@ def main():
             else:
                 column.pop(0)
                 column = list(map(lambda x: round(x,3),list(map(float,column))))
-                for i in range(len(column)-1):
-                    DataCsv[(dataset*4)+int((count%12-4)/2-0.5)][i+1] += column[i]
-                #DataCsv[(dataset*4)+int((count%12-4)/2-0.5)] += list(map(lambda x: round(x,3),list(map(float,column))))
+                print(len(column))
+                if len(column) !=1:
+                    for i in range(len(column)-1):
+                        #print((dataset*5)+int((count%12-2)/2-1.5))
+                        DataCsv[(dataset*5)+int((count%12-2)/2-0.5)][i+1] += column[i]
+                    #DataCsv[(dataset*5)+int((count%12-4)/2-0.5)] += list(map(lambda x: round(x,3),list(map(float,column))))
+                else:
+                    DataCsv[(dataset*5)+int((count%12-2)/2-0.5)][1] += column[0]
         count +=1
     for Data in range(len(DataCsv)):
-        try:
+        #try:
             tmpname = DataCsv[Data].pop(0)
-            for i in range(len(DataCsv[Data])-1):
-                DataCsv[Data][i]/=10
-            DataCsv[Data] = list(map(lambda x: round(x,3),list(map(float,DataCsv[Data]))))
+            if len(DataCsv[Data]) !=1:
+                for i in range(len(DataCsv[Data])):
+                    DataCsv[Data][i-1]/=10
+                DataCsv[Data] = list(map(lambda x: round(x,3),list(map(float,DataCsv[Data]))))
+            else:
+                DataCsv[Data][0]/=10
             DataCsv[Data].insert(0,tmpname)
-        except:
-            print("empty row")
+        #except:
+         #   print("empty row")
     #print(DataCsv)
     #for data in DataCsv:  
     writer.writerows(DataCsv)
